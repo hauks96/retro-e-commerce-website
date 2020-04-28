2677c6554b308b61555bee84eaa7e5d23ccffbbc
arnar-steinthors <arnardadi99@hotmail.com>
2020-04-28 15:44:09 +0000

    login page and more packages for venv

--- captain_console/store/urls.py
+++ captain_console/store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="Front Page!")
+    path('', views.index, name="Front Page!"),
+    path('login', views.login)
 ]
--- captain_console/store/views.py
+++ captain_console/store/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 # home page view (index)
 def index(request):
     return render(request, "store/index.html")
+
+# login page view ( /login )
+def login(request):
+    return render(request, "store/login.html")
+
