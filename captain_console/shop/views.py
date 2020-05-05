25283481b54955fa4e792b49447330f4f5bd6ac7
Hauks96 <agir96@gmail.com>
2020-05-05 12:14:53 +0000

    Added filter quieries to shop view

--- captain_console/shop/views.py
+++ captain_console/shop/views.py
@@ -1,29 +1,92 @@
 from django.shortcuts import render
-from shop.models import Product #importar products
+from shop.models import Product  # imports products
 # Create your views here.
+from user.models import User
+
 
 def shop(request):
-    if 'search_filter' in request:
-        #filter by search filter
-        pass
-    elif 'product_type' in request:
-        #filter by product type
-        pass
-    elif 'by_name' in request:
-        #filter by name value asc/desc
-        pass
-    else:
-        #return all data
-        pass
+    context = {}
+    if 'search_filter' in request.GET:
+        search = request.GET['search_filter']
 
-    return render(request, 'shop/shop.html', context={"products": Product.objects.all()})
+        if 'by_name' in request.GET:
+            by_name = request.GET['by_name']  # Asc or Desc
+            if by_name == 'asc':
+                context["products"] = Product.objects.filter(name__icontains=search).order_by('-name')
+            else:
+                context["products"] = Product.objects.filter(name__icontains=search).order_by('name')
+
+        elif 'by_price' in request.GET:
+            by_price = request.GET['by_price']  # Asc or Desc
+            if by_price == 'asc':
+                context["products"] = Product.objects.filter(name__icontains=search).order_by('-price')
+            else:
+                context["products"] = Product.objects.filter(name__icontains=search).order_by('price')
+
+        else:
+            context["products"] = Product.objects.filter(name__icontains=search)
+
+    if 'product_type' in request.GET:
+        prod_type = request.GET['product_type']
+        if context:
+            for item in context['product']:
+                if item.type != prod_type:
+                    del item
+
+        else:
+            if 'by_name' in request.GET:
+                by_name = request.GET['by_name']  # Asc or Desc
+                if by_name == 'asc':
+                    context["products"] = Product.objects.filter(type=prod_type).order_by('-name')
+                else:
+                    context["products"] = Product.objects.filter(type=prod_type).order_by('name')
+
+            elif 'by_price' in request.GET:
+                by_price = request.GET['by_price']  # Asc or Desc
+                if by_price == 'asc':
+                    context["products"] = Product.objects.filter(type=prod_type).order_by('-price')
+                else:
+                    context["products"] = Product.objects.filter(type=prod_type).order_by('price')
+
+            else:
+                context['product'] = Product.objects.filter(type=prod_type)
+
+    elif 'by_name' in request.GET:
+        by_name = request.GET['by_name']  # Asc or Desc
+        if by_name == 'asc':
+            context['product'] = Product.objects.all().order_by('-name')
+        else:
+            context['product'] = Product.objects.all().order_by('name')
+
+    elif 'by_price' in request.GET:
+        by_price = request.GET['by_price']  # Asc or Desc
+        if by_price == 'asc':
+            context["products"] = Product.objects.all().order_by('-price')
+        else:
+            context["products"] = Product.objects.all().order_by('price')
+
+    else:
+        context['products'] = Product.objects.all()
+
+    return render(request, 'shop/shop.html', context)
+
 
 def product(request, product_id):
-    #fetch product data or return 404
-    #if user.is_authenticated -> save search to search hisory model
-    #load product details page
+    # fetch product data or return 404
+    # if user.is_authenticated -> save search to search hisory model
+    # load product details page
     return render(request, 'shop/product.html')
 
 
+def add_to_basket(request):
+    pass
+    """
+    value = request.COOKIES.get('cart')
+    if value is None:
+        # Cookie is not set
+        response = render(request, template_name='user/shop.html')
 
-
+    if request.user.is_authenticated:
+        user = User.objects.get(id=request.user.id)
+        basket = user.cart
+    """
