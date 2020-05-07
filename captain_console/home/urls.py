290a63999c686ff496a258237450559d8ec26fa5
aiceboy <54935942+aiceboy@users.noreply.github.com>
2020-05-07 10:46:31 +0000

    Added 'useful links' templates
    ...

--- captain_console/home/urls.py
+++ captain_console/home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home-index"),
+    path('affiliate/', views.affiliate, name="affiliate"),
+    path('shipping/', views.shipping, name='shipping'),
 ]
