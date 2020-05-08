6b7720710be4078c5b1cb3b8b3e2457c61af5265
aiceboy <54935942+aiceboy@users.noreply.github.com>
2020-05-08 22:40:20 +0000

    Checkout progress
    ...

--- captain_console/order/forms.py
+++ captain_console/order/forms.py
@@ -4,7 +4,7 @@ from user.models import Address
 from django.db import models
 
 
-class ShippingAddressForm(ModelForm):
+class ShippingAddressForm(ModelForm): #To use if user is logged in and wants to use his saved address info
     class Meta:
         model = Address
         exclude = ['id']
@@ -17,11 +17,21 @@ class ShippingAddressForm(ModelForm):
         }
 
 
+class ShippingAddressInfoForm(forms.Form):
+    address = forms.CharField(label='Please enter your Address', max_length=100, required=True)
+    country = forms.CharField(label='Please enter your Country', max_length=50, required=True)
+    city = forms.CharField(label='Please enter your City', max_length=50, required=True)
+    postal_code = forms.CharField(label='Please enter your Postal Code', max_length=20, required=True)
+    note = forms.CharField(label='Enter additional info if required', max_length=25, required=False)
+
+
 class PaymentInfoForm(forms.Form):
-    cardholder_name = forms.CharField(label='Please enter cardholder name', max_length=20, required=True),
-    credit_card_num = forms.CharField(label='Please enter your credit card number', max_length=20, required=True),
-    expiry_date = forms.NumberInput()
-    CVC = forms.NumberInput()
+    # If forms.NumberInput() is used the form does not show on the template
+    cardholder_name = forms.CharField(label='Please enter cardholder name', max_length=100, required=True)
+    credit_card_num = forms.CharField(label='Please enter credit card number', max_length=100, required=True)
+    expiry_date = forms.CharField(label='Please enter expiry date', max_length=100, required=True)
+    CVC = forms.CharField(label='Please enter 3 digit CVC number on the back of your card', max_length=100, required=True)
+
 
 
 
--- captain_console/order/views.py
+++ captain_console/order/views.py
@@ -1,11 +1,85 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from user.models import Address, User
-from .forms import ShippingAddressForm, PaymentInfoForm
+from shop.models import Product, ProductImage
+from cart.views import render_dict_cookie
+from .forms import ShippingAddressForm, PaymentInfoForm, ShippingAddressInfoForm
 
 # Create your views here.
 
 def shipping(request):
     context = {}
+    my_form = ShippingAddressInfoForm()
+    if request.method == "POST":
+        my_form = ShippingAddressInfoForm(request.POST)
+        if my_form.is_valid():
+            request.session['address'] = my_form.cleaned_data['address']
+            request.session['country'] = my_form.cleaned_data['country']
+            request.session['city'] = my_form.cleaned_data['city']
+            request.session['postal_code'] = my_form.cleaned_data['postal_code']
+            request.session['note'] = my_form.cleaned_data['note']
+            return redirect('../payment/')
+        else:
+            my_form = ShippingAddressInfoForm()
+    context = {'form': my_form}
+    return render(request, 'order/shippingInfo.html', context)
+
+
+def billing(request):
+    my_form = PaymentInfoForm()
+    if request.method == "POST":
+        my_form = PaymentInfoForm(request.POST)
+        if my_form.is_valid():
+            my_form = my_form.cleaned_data
+            cardholder_name = my_form['cardholder_name']
+            credit_card_num = my_form['credit_card_num']
+            expiry_date = my_form['expiry_date']
+            CVC = my_form['CVC']
+            return redirect(
+                '../summary/?cardholder_name=' + cardholder_name
+                + '&credit_card_num=' + credit_card_num + '&expiry_date=' + expiry_date + '&CVC=' + CVC)
+        else:
+            my_form = PaymentInfoForm()
+    context = {'form': my_form}
+    return render(request, 'order/paymentInfo.html', context)
+
+def summary(request):
+    print('KingKongBitch')
+    print(request.session['address'])
+    #TODO addContext
+    # Initializing form list, since we are rendering multiple forms
+    cart_cookie = request.COOKIES['cart']
+    cart_dict = render_dict_cookie(cart_cookie)
+    cart_keys = list(cart_dict.keys())
+    productObjList = []
+
+    # Sending a cart total value with the return context (call by name in template)
+    cart_total = 0
+
+    # Iterating over all the keys in the cart dictionary
+    for product_id in cart_keys:
+        quantity = int(cart_dict[str(product_id)])
+        # Trying to fetch a product with the product id present in the cookie dict
+        try:
+            # Setting all return data values
+            product = Product.objects.get(id=product_id)
+            productObjList.append(product)
+            product_image = ProductImage.objects.filter(product=product.id).first()
+            cart_total += product.price * quantity
+
+        # If the product we tried to fetch didn't exist we skip it and delete the key
+        except Product.DoesNotExist:
+            del cart_dict[product_id]
+            continue
+
+    return render(request, 'order/summaryPage.html', context={'cart_total': cart_total, 'productList': productObjList})
+
+
+def success(request):
+    return render(request, 'order/confirmationPage.html')
+
+    #@login_required() - TODO For using when user wants to use stored address
+"""context = {}
     user = get_object_or_404(User, pk=request.user.id)
     address = user.address
     form = ShippingAddressForm(instance=address)
@@ -15,32 +89,4 @@ def shipping(request):
             form.save()
             #return redirect('billing-index') TODO redirect on to payment
     context['form'] = form
-    return render(request, 'order/shippingInfo.html', context)
-
-
-def billing(request): #TODO fix so that form renders in template
-    form = PaymentInfoForm()
-    context = {'form': form}
-    print(request.method)
-    if request.method == "POST":
-        print(request.method)
-        #form = PaymentInfoForm(request.POST)
-        if form.is_valid():
-            print(form.is_valid())
-            print(form.cleaned_data)
-            context = {'form': form}
-            #form.save()
-            pass
-    else:
-        form = PaymentInfoForm()
-        #print(form.errors)
-    #context['form'] = form"""
-    return render(request, 'order/paymentInfo.html', context)
-
-def summary(request):
-    #TODO addContext
-    return render(request, 'order/summaryPage.html')
-
-
-def success(request):
-    return render(request, 'order/confirmationPage.html')
+    return render(request, 'order/shippingInfo.html', context)"""
